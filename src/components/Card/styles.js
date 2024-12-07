2efcec63f30fa9d314ae81b1c088a7843d901da8
vhraposo <victorhcraposo@gmail.com>
2024-12-07 01:58:08 -0300

    feat: update Card component styles for improved layout and responsiveness

--- src/components/Card/styles.js
+++ src/components/Card/styles.js
@@ -4,14 +4,15 @@ export const Container = styled.div`
   background-color: ${(props) => props.backgroundColor || '#0055a4'};
   color: #ffffff;
   border-radius: 5px;
-  width: 20rem;
-  height: 10rem;
+  width: 30rem;
+  height: auto;
   display: flex;
   flex-direction: column;
   justify-content: space-between;
-  padding: 1.5rem;
+  padding: 2rem;
   box-shadow: 0px 4px 6px rgba(0, 0, 0, 0.1);
   margin-top: 2rem;
+  position: relative;
 `;
 
 export const Title = styled.span`
@@ -31,13 +32,17 @@ export const Number = styled.span`
 `;
 
 export const IconWrapper = styled.div`
-  width: 2rem;
-  height: 2rem;
+  width: 4.5rem;
+  height: 4.5rem;
   border-radius: 50%;
   background-color: ${(props) => props.iconColor || '#00cc44'};
   display: flex;
   justify-content: center;
   align-items: center;
   color: white;
-  font-size: 1.4rem;
+  font-size: 2.5rem;
+  position: absolute;
+  top: 0;
+  right: 0;
+  transform: translate(-10%, 15%);
 `;
